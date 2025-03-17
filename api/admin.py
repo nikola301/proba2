07641ba9ca70c025d9efa1d82afb39c58469785c
nikola301 <153811019+nikola301@users.noreply.github.com>
2025-03-17 21:06:20 +0100

    Delete api directory

--- api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from api.models import Helikopteri
-
-# Register your models here.
-
-admin.site.register(Helikopteri)
--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Helikopteri
-
-class HelikopteriSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Helikopteri
-        fields = '__all__'  # Includes all fields
